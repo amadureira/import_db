035562c1c49496832c5b75e6b0a507804644d8d4
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2018-06-07 18:32:37 -0300

    last update

--- start.py
+++ start.py
@@ -1,7 +1,14 @@
-import db.sqlserver.connect
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
 
+import db.sqlserver.connect
+sql="SELECT * FROM COLECAO  where Colecao.idColecao = 20";
+table= "COLECAO"
 db = db.sqlserver.connect.SQL();
 #db.createTable(db.getTables("DB_DES_CASASBAHIA_VIA_UNICA_MOBILE"));
 jsConfig = db.getRelation("DB_DES_CASASBAHIA_VIA_UNICA_MOBILE");
-#print(jsConfig)
-db.createQuery("CLIENTE",jsConfig);
+#print(jsConfig);
+jsVal = db.createQuery(sql,table,jsConfig);
+#jsVal = db.getSelect("SELECT * FROM COLECAO  where Colecao.idColecao BETWEEN 20 and  28");
+print(jsVal);
+
